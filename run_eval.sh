d7dbca592bdcab46e5c29da275e98089b26239a4
Shun14 <zzsshun13@gmail.com>
2021-08-19 00:34:05 +0800

    Update run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -1,1 +1,1 @@
-export CUDA_VISIBLE_DEVICES=0 python val.py --config configs/pfpn/pfpn_resnet101_os8_cityscapes_512x1024_40k.yml  --model_path pfpnnet_cityscapes_b8/best_model/model.pdparams
+CUDA_VISIBLE_DEVICES=0 python val.py --config configs/pfpn/pfpn_resnet101_os8_cityscapes_512x1024_40k.yml  --model_path pfpnnet_cityscapes_b8/best_model/model.pdparams
